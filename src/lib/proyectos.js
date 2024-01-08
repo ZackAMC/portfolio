e7cc125f19b098cc977a00cb5d3306f1d9b6db97
ZackAMC <andresmila01@gmail.com>
2024-01-07 19:38:18 -0500

    Avances en el orden de los iconos, logos  y nav.

---
+++ src/lib/proyectos.js
@@ -0,0 +1,136 @@
+const proyectos = [
+    {
+        "nombre": "Audi - Colombia",
+        "tecnologias": [
+            "WordPress",
+            "JavaScript",
+            "PHP"
+        ],
+        "descripcion": "El proyecto Audi - Colombia, consistía principalmente en realizar el sitio web en WordPress, de gran importancia, ya que futuros proyectos dependían del resultado de este sitio web.",
+        "img" : "audi-col.jpg",
+        "link": "https://colwagen.com/audi/"
+    },
+    {
+        "nombre": "Finazul",
+        "tecnologias": [
+            "WordPress",
+            "JavaScript",
+            "PHP"
+        ],
+        "descripcion": "Finazul fue un proyecto sencillo planteado en WordPress; La idea es un sitio web informativo, pequeño, en el cual el cliente tuviese facilidad para contactarse con Finazul para adquirir el seguro deseado.",
+        "img" : "finazul.jpg",
+        "link": "https://finazul.co/"
+    },
+    {
+        "nombre": "Seguros A&S",
+        "tecnologias": [
+            "JavaScript",
+            "PHP",
+            "Zoho"
+        ],
+        "descripcion": "Seguros A&S es un proyecto el cual fue planteado para realizar únicamente con PHP, esto para lograr el mayor performance.",
+        "img" : "ays.jpg",
+        "link": "https://segurosays.com/"
+    },
+    {
+        "nombre": "Marketeros Agencia",
+        "tecnologias": [
+            "JavaScript",
+            "PHP",
+            "Zoho"
+        ],
+        "descripcion": "Construido con PHP, el sitio web Marketeros Agencia fue un proyecto cuyo objetivo es brindar información detallada del amplio portafolio para sus clientes, haciendo énfasis en Inbound Marketing y Zoho.",
+        "img" : "Marketeros.jpg",
+        "link": "https://www.marketerosagencia.com/"
+    },
+    {
+        "nombre": "Cynthus",
+        "tecnologias": [
+            "WordPress",
+            "JavaScript",
+            "PHP"
+        ],
+        "descripcion": "Cynthus es un e-commerce destinado a una amplia variedad de servicios como asesorías, auditorías y cursos, principalmente destinados a la ciberseguridad.",
+        "img" : "Cynthus.jpg",
+        "link": "https://www.cynthus.com.mx/"
+    },
+    {
+        "nombre": "Fernanda Mila Spa",
+        "tecnologias": [
+            "WordPress",
+            "JavaScript",
+            "PHP"
+        ],
+        "descripcion": "Fernanda Mila Spa es un e-commerce destinado a la venta de tratamientos y productos para el cuidado, reducción y moldeo corporal.",
+        "img" : "FernandaMilaSpa.jpg",
+        "link": "https://fernandamilaspa.com.co/"
+    },
+    {
+        "nombre": "DSI Arquitectos",
+        "tecnologias": [
+            "WordPress",
+            "JavaScript",
+            "PHP"
+        ],
+        "descripcion": "Sitio web informativo, desarrollado en WordPress, su principal función es exponer sus servicios al público y funcionar como puente de comunicación para el cliente con la empresa.",
+        "img" : "DSI.jpg",
+        "link": "https://dsiarquitectos.com/"
+    },
+    {
+        "nombre": "Terracore",
+        "tecnologias": [
+            "WordPress",
+            "JavaScript",
+            "PHP"
+        ],
+        "descripcion": "Sitio web informativo, desarrollado en WordPress, su principal función es exponer sus servicios al público y funcionar como puente de comunicación para el cliente con la empresa.",
+        "img" : "Terracore.jpg",
+        "link": "https://terracorecap.com/"
+    },
+    {
+        "nombre": "Pelecanus",
+        "tecnologias": [
+            "WordPress",
+            "JavaScript",
+            "PHP"
+        ],
+        "descripcion": "Sitio web multiidioma, desarrollado en WordPress con la particularidad de no utilizar ningún constructor como WPBakery o Elementor, fue construido con Shortcodes los cuales aplican los elementos HTML.",
+        "img" : "Pelecanus.jpg",
+        "link": "https://pelecanus.com.co/es/"
+    },
+    {
+        "nombre": "Viaje en Marruecos",
+        "tecnologias": [
+            "WordPress",
+            "JavaScript",
+            "PHP"
+        ],
+        "descripcion": "Sitio web informativo, desarrollado en WordPress, su principal funcion es exponer sus servicios al público y funcionar como puente de comunicación para el cliente con la empresa.",
+        "img" : "ViajeenMarruecos.jpg",
+        "link": "https://viajeenmarruecos.com/"
+    },
+    {
+        "nombre": "App control de gastos",
+        "tecnologias": [
+            "React",
+            "Vite"
+        ],
+        "descripcion": "Esta app es parte del curso de React, la cual tiene como objetivo y como su nombre lo indica, generar un control de gastos; Hecho con ReactJs y VITE.",
+        "img" : "controlGastos.jpg",
+        "link": "https://github.com/ZackAMC/control-gastos"
+    },
+    {
+        "nombre": "Seguimiento de Pacientes",
+        "tecnologias": [
+            "ReactJs",
+            "Vite"
+        ],
+        "descripcion": "App de práctica en React cuyo objetivo es generar seguimiento a pacientes de una veterinaria.",
+        "img" : "seguimientoPacientes.jpg",
+        "link": "https://github.com/ZackAMC/citas_react_vite"
+    }
+]
+
+export {
+    proyectos
+}
