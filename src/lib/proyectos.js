ca0546b720ec6b4a75dccb2cc47511b660bb6891
ZackAMC <andresmila01@gmail.com>
2024-01-11 07:51:11 -0500

    Animaciones Proyectos

--- src/lib/proyectos.js
+++ src/lib/proyectos.js
@@ -7,7 +7,7 @@ const proyectos = [
             "PHP"
         ],
         "descripcion": "El proyecto Audi - Colombia, consistía principalmente en realizar el sitio web en WordPress, de gran importancia, ya que futuros proyectos dependían del resultado de este sitio web.",
-        "img" : "audi-col.jpg",
+        "img" : "audi-col",
         "link": "https://colwagen.com/audi/"
     },
     {
@@ -18,7 +18,7 @@ const proyectos = [
             "PHP"
         ],
         "descripcion": "Finazul fue un proyecto sencillo planteado en WordPress; La idea es un sitio web informativo, pequeño, en el cual el cliente tuviese facilidad para contactarse con Finazul para adquirir el seguro deseado.",
-        "img" : "finazul.jpg",
+        "img" : "finazul",
         "link": "https://finazul.co/"
     },
     {
@@ -29,7 +29,7 @@ const proyectos = [
             "Zoho"
         ],
         "descripcion": "Seguros A&S es un proyecto el cual fue planteado para realizar únicamente con PHP, esto para lograr el mayor performance.",
-        "img" : "ays.jpg",
+        "img" : "ays",
         "link": "https://segurosays.com/"
     },
     {
@@ -40,7 +40,7 @@ const proyectos = [
             "Zoho"
         ],
         "descripcion": "Construido con PHP, el sitio web Marketeros Agencia fue un proyecto cuyo objetivo es brindar información detallada del amplio portafolio para sus clientes, haciendo énfasis en Inbound Marketing y Zoho.",
-        "img" : "Marketeros.jpg",
+        "img" : "Marketeros",
         "link": "https://www.marketerosagencia.com/"
     },
     {
@@ -51,7 +51,7 @@ const proyectos = [
             "PHP"
         ],
         "descripcion": "Cynthus es un e-commerce destinado a una amplia variedad de servicios como asesorías, auditorías y cursos, principalmente destinados a la ciberseguridad.",
-        "img" : "Cynthus.jpg",
+        "img" : "Cynthus",
         "link": "https://www.cynthus.com.mx/"
     },
     {
@@ -62,7 +62,7 @@ const proyectos = [
             "PHP"
         ],
         "descripcion": "Fernanda Mila Spa es un e-commerce destinado a la venta de tratamientos y productos para el cuidado, reducción y moldeo corporal.",
-        "img" : "FernandaMilaSpa.jpg",
+        "img" : "FernandaMilaSpa",
         "link": "https://fernandamilaspa.com.co/"
     },
     {
@@ -73,7 +73,7 @@ const proyectos = [
             "PHP"
         ],
         "descripcion": "Sitio web informativo, desarrollado en WordPress, su principal función es exponer sus servicios al público y funcionar como puente de comunicación para el cliente con la empresa.",
-        "img" : "DSI.jpg",
+        "img" : "DSI",
         "link": "https://dsiarquitectos.com/"
     },
     {
@@ -84,7 +84,7 @@ const proyectos = [
             "PHP"
         ],
         "descripcion": "Sitio web informativo, desarrollado en WordPress, su principal función es exponer sus servicios al público y funcionar como puente de comunicación para el cliente con la empresa.",
-        "img" : "Terracore.jpg",
+        "img" : "Terracore",
         "link": "https://terracorecap.com/"
     },
     {
@@ -95,7 +95,7 @@ const proyectos = [
             "PHP"
         ],
         "descripcion": "Sitio web multiidioma, desarrollado en WordPress con la particularidad de no utilizar ningún constructor como WPBakery o Elementor, fue construido con Shortcodes los cuales aplican los elementos HTML.",
-        "img" : "Pelecanus.jpg",
+        "img" : "Pelecanus",
         "link": "https://pelecanus.com.co/es/"
     },
     {
@@ -106,7 +106,7 @@ const proyectos = [
             "PHP"
         ],
         "descripcion": "Sitio web informativo, desarrollado en WordPress, su principal funcion es exponer sus servicios al público y funcionar como puente de comunicación para el cliente con la empresa.",
-        "img" : "ViajeenMarruecos.jpg",
+        "img" : "ViajeenMarruecos",
         "link": "https://viajeenmarruecos.com/"
     },
     {
@@ -115,18 +115,18 @@ const proyectos = [
             "React",
             "Vite"
         ],
-        "descripcion": "Esta app es parte del curso de React, la cual tiene como objetivo y como su nombre lo indica, generar un control de gastos; Hecho con ReactJs y VITE.",
-        "img" : "controlGastos.jpg",
+        "descripcion": "Esta app es parte del curso de React, la cual tiene como objetivo y como su nombre lo indica, generar un control de gastos; Hecho con React y VITE.",
+        "img" : "controlGastos",
         "link": "https://github.com/ZackAMC/control-gastos"
     },
     {
         "nombre": "Seguimiento de Pacientes",
         "tecnologias": [
-            "ReactJs",
+            "React",
             "Vite"
         ],
         "descripcion": "App de práctica en React cuyo objetivo es generar seguimiento a pacientes de una veterinaria.",
-        "img" : "seguimientoPacientes.jpg",
+        "img" : "seguimientoPacientes",
         "link": "https://github.com/ZackAMC/citas_react_vite"
     }
 ]
